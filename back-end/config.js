cfc9320195c2b6b95cdd6b6bbc4c2e89bd3d064d
Waffog <93392822+ToPGaMiSkS@users.noreply.github.com>
2023-10-09 20:20:21 -0300

    Create config.js

---
+++ back-end/config.js
@@ -0,0 +1,12 @@
+const stylus = require('stylus');
+const cookieParser = require('cookie-parser');
+
+module.exports = function(app, express) {
+
+	app.use(cookieParser());
+	app.use(stylus.middleware({src: __dirname + '/public'}));
+	app.use(express.static(__dirname + '/public'));
+	require(__dirname + '/server/model/database')(app);
+	require(__dirname + '/server/routes')(app);
+
+}
